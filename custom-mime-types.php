dfdc2f0da5e1de3eea790026c94afd7ec5c26c9f
Jafran Hasan <exjafran@gmail.com>
2021-07-31 10:31:36 +0600

    added readme.txt

--- custom-mime-types.php
+++ custom-mime-types.php
@@ -1,18 +1,24 @@
-<?php 
+<?php  
+
 /**
- * Plugin name: Custom Mime Types
- * Plugin URI: #
- * Author: Jafran
- * Author URI: https://facebook.com/iamjafran
- * Description: Customize allowed uploadable file extention within a single click, upload various type of files on your website. Rolewise permission can be set. 
- * Textdomain: custommimetypes
+ * Plugin Name:       Custom Mime Types
+ * Plugin URI:        https://facebook.com/iamjafran/
+ * Description:       Customize allowed uploadable file extention within a single click, upload various type of files on your website. Set role permissions and maximum upload file size in one place. 
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      5.4
+ * Author:            Jafran Hasan
+ * Author URI:        https://facebook.com/iamjafran/
+ * Text Domain:       evr
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
  
 
 defined( 'ABSPATH' ) or die();
 
 /** 
- * Define 
+ * Define handler file
  */
 define( 'CMT_FILE' , __FILE__);
 
@@ -20,3 +26,8 @@ define( 'CMT_FILE' , __FILE__);
  * Loading the app
  */
 require_once plugin_dir_path( CMT_FILE ) . 'includes/class/class-app.php';
+
+/**
+ * Custom Mime Type Developed by Jafran Hasan
+ * This is a freeware
+ */
